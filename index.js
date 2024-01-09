e8f27e5fe67c935e13741af5d2738a792a6f5842
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-01-09 12:49:00 +0530

    refactor: change the function according to condition

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function setSpeed() {
     const speedInput = document.getElementById('speedInput');
     const speed = parseInt(speedInput.value);
 
-    if (speed >= 0 && speed <= 180) {
+    if (speed >= 0 && speed <= 100) {
       const needle = document.getElementById('needle');
-      const angle = (speed / 180) * 180 - 90;
+      const angle = 180 * speed/100 - 90;
       needle.style.transform = `translateX(-50%) rotate(${angle}deg)`;
     } else {
-      alert('Please enter a valid speed between 0 and 180 km/h.');
+      alert('Please enter a valid speed between 0 and 100 km/h.');
     }
   }
