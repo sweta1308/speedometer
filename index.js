d7fb27eb100760d4fee32f1bbf8c423381b96248
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-01-09 15:20:37 +0530

    fix: bug

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function setSpeed() {
       const needle = document.getElementById('needle');
       const angle = 270 * speed/180 - 135;
       needle.style.transform = `translateX(-50%) rotate(${angle}deg)`;
-      speedInput.value = 0
     } else {
       alert('Please enter a valid speed between 0 and 180 km/h.');
     }
