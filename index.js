966c9cfb1c5a4d10279b7f82d45d4f52b6535759
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-01-09 13:06:59 +0530

    refactor: changed the limits of speed

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function setSpeed() {
     const speedInput = document.getElementById('speedInput');
     const speed = parseInt(speedInput.value);
 
-    if (speed >= 0 && speed <= 100) {
+    if (speed >= 0 && speed <= 180) {
       const needle = document.getElementById('needle');
-      const angle = 180 * speed/100 - 90;
+      const angle = 270 * speed/180 - 135;
       needle.style.transform = `translateX(-50%) rotate(${angle}deg)`;
     } else {
-      alert('Please enter a valid speed between 0 and 100 km/h.');
+      alert('Please enter a valid speed between 0 and 180 km/h.');
     }
   }
